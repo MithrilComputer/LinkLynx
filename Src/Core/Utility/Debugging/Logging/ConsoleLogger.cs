d09352d4d55c36d50b0cd1857854e3e847e31701
MithrilComputer <mithrilcomputers@gmail.com>
2025-08-17 22:28:55 -0400

    Addition
    Added A new logger

---
+++ Src/Core/Utility/Debugging/Logging/ConsoleLogger.cs
@@ -0,0 +1,15 @@
+﻿using Crestron.SimplSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkLynx.Core.Utility.Debugging.Logging
+{
+    public static class ConsoleLogger
+    {
+        public static void Log(string message) => CrestronConsole.PrintLine(message);
+    }
+}
