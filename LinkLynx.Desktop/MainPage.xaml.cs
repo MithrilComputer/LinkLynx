966d3918d6a45382ebdd3319dfb6d549b5b95ded
MithrilComputer <mithrilcomputers@gmail.com>
2026-08-21 16:01:18 -0400

    Inital Project Creation and Organization

---
+++ LinkLynx.Desktop/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkLynx.Desktop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LinkLynx.Desktop/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LinkLynx.Desktop.Platforms.MacCatalyst
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
